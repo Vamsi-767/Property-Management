3d3e29a91a66c7ef4b6c6bcec209109e2480c36c
Vamsi Krishna Bodaballa <vamsikrishnabodaballa7@gmail.com>
2024-11-05 17:37:50 -0600

    Implemented Error handling using AOP

--- src/main/java/com/mycompany/property_management/Repository/UserRepository.java
+++ src/main/java/com/mycompany/property_management/Repository/UserRepository.java
@@ -2,5 +2,9 @@ package com.mycompany.property_management.Repository;
 import com.mycompany.property_management.DTO.Entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
+    Optional<UserEntity> findByOwnerEmailAndPassword(String email,String password);
+    Optional<UserEntity> findByOwnerEmail(String email);
 }
--- src/main/java/com/mycompany/property_management/Service/Impl/UserServiceImpl.java
+++ src/main/java/com/mycompany/property_management/Service/Impl/UserServiceImpl.java
@@ -5,8 +5,14 @@ import com.mycompany.property_management.DTO.Entity.UserEntity;
 import com.mycompany.property_management.DTO.UserDTO;
 import com.mycompany.property_management.Repository.UserRepository;
 import com.mycompany.property_management.Service.UserService;
+import com.mycompany.property_management.exception.BusinessException;
+import com.mycompany.property_management.exception.ErrorModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,6 +22,17 @@ public class UserServiceImpl implements UserService {
     private UserConverter userConverter;
     @Override
     public UserDTO register(UserDTO userDTO) {
+        Optional<UserEntity> oue=userRepository.findByOwnerEmail(userDTO.getOwnerEmail());
+        if(oue.isPresent())
+        {
+            List<ErrorModel> errorModelList=new ArrayList<>();
+            ErrorModel errorModel=new ErrorModel();
+            errorModel.setCode("EMAIL_ALREADY_EXISTS");
+            errorModel.setMessage("Please enter a unique email");
+            errorModelList.add(errorModel);
+            throw new BusinessException(errorModelList);
+
+        }
         UserEntity userEntity=userConverter.convertDTOtoEntity(userDTO);
         userEntity=userRepository.save(userEntity);
         userDTO=userConverter.convertEntitytoDTO(userEntity);
@@ -24,6 +41,22 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO login(String email, String password) {
-        return null;
+        UserDTO userDTO=null;
+        Optional<UserEntity> optionalUserEntity=userRepository.findByOwnerEmailAndPassword(email,password);
+        if(optionalUserEntity.isPresent())
+        {
+            userDTO=userConverter.convertEntitytoDTO(optionalUserEntity.get());
+        }
+        else {
+            List<ErrorModel> errorModelList=new ArrayList<>();
+            ErrorModel errorModel=new ErrorModel();
+            errorModel.setCode("INVALID_LOGIN");
+            errorModel.setMessage("Incorrect Username/Password");
+            errorModelList.add(errorModel);
+            throw new BusinessException(errorModelList);
+
+        }
+
+        return userDTO;
     }
 }
